a2b3ecd259ad1e7529f6aa03277fef11703ff27c
OscarD8 <oscardavison8@outlook.com>
2025-08-13 10:19:07 +0100

    BottomNavBar started

--- app/src/main/java/com/example/mefood/MeFoodScreen.kt
+++ app/src/main/java/com/example/mefood/MeFoodScreen.kt
@@ -1,10 +1,15 @@
 package com.example.mefood
 
+import android.content.Context
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bento
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
@@ -15,7 +20,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHost
@@ -30,10 +38,11 @@ import com.example.mefood.ui.MenuScreen
 
 enum class MeFoodScreen(
     @StringRes val title: Int,
-    @StringRes val route: Int
+    @StringRes val route: Int,
+    val icon: ImageVector
 ) {
-    Home(R.string.app_name, R.string.home_route_name),
-    Starter(R.string.starter_header, R.string.starter_route_name)
+    Home(R.string.app_name, R.string.home_route_name, Icons.Default.Home),
+    Starter(R.string.starter_header, R.string.starter_route_name, Icons.Default.Bento)
 }
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -52,24 +61,32 @@ private fun TopAppBar(
     )
 }
 
-@Composable
-private fun BottomNavBar(
-    navController: NavHostController
-) {
-    val startDestination = MeFoodScreen.Home
-    val selectedDestination by remember { mutableIntStateOf(startDestination) }
-
-    NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
-        MeFoodScreen.entries.forEach { index, destination ->
-            NavigationBarItem(
-                selected = selectedDestination == index,
-                onClick = {
-                    navController.navigate(route = destination.rou)
-                }
-            )
-        }
-    }
-}
+//@Composable
+//private fun BottomNavBar(
+//    navController: NavHostController,
+//    context: Context
+//) {
+//    val startDestination = MeFoodScreen.Home
+//    var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+//
+//
+//    NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+//        MeFoodScreen.entries.forEachIndexed { index, destination ->
+//            NavigationBarItem(
+//                selected = selectedDestination == index,
+//                onClick = {
+//                    navController.navigate(route = context.getString(destination.route))
+//                },
+//                icon = {
+//                    Icon(
+//                        destination.icon,
+//                        contentDescription = null // TODO
+//                    )
+//                }
+//            )
+//        }
+//    }
+//}
 
 @Composable
 fun MeFoodApp() {
@@ -90,9 +107,19 @@ fun MeFoodApp() {
             startDestination = MeFoodScreen.Home.name,
             modifier = Modifier.padding(innerPadding)
         ) {
-
             MeFoodScreen.entries.forEach { destination ->
-                composable(destination.)
+                composable(context.resources.getString(destination.route)) {
+                    when (destination) {
+                        MeFoodScreen.Home -> HomeScreen(
+                            onClick = { navController.navigate(MeFoodScreen.Starter) },
+                            modifier = Modifier.fillMaxSize()
+                        )
+                        MeFoodScreen.Starter -> MenuScreen(
+                            foods = Datasource.foods,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                }
             }
         }
     }
